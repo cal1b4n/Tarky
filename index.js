6e90d724bfe76528dccb8fc7d20eda18ec580558
Saba Sakvarelidze <44819695+cal1b4n@users.noreply.github.com>
2020-12-28 08:41:33 -0800

    Added test commands & re-wrote the index.js file

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require('./config.json')
+const command = require('./commands/commands.js')
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+client.on('ready', () => {
+	console.log('Tarky is Ready!')
 
-client.on('message', message => {
-	if (message.content === '!ping') {
-		message.channel.send('Pong.');
-	}
-});
+	// Ping <-> Pong test
+	command(client, ['ping', 'test'], message => {
+		message.reply('Pong!')
+	})
 
-client.login(config.token);
+	// shows member count of every server the bot is on
+	command(client, 'servers', message => {
+		client.guilds.cache.forEach(guild => {
+			message.reply(`${guild.name} has a total of ${guild.memberCount} members`)
+		})
+	})
+
+	// why not?
+	command(client, 'joke', message => {
+		message.reply('your life is enough of a joke :recycle: ')
+	})
+
+	// clears all the messages in the channel (Only if user has ADMINISTRATOR role)
+	command(client, 'cc', message => {
+		if (message.member.hasPermission('ADMINISTRATOR')) {
+			message.channel.messages.fetch().then(results => {
+				message.channel.bulkDelete(results)
+			})
+		}
+	})
+
+
+	// change the bot status (Only if user has ADMINISTRATOR role)
+	command(client, 'status', message => {
+		if (message.member.hasPermission('ADMINISTRATOR')){
+			const content = message.content.replace('!status ', '')
+
+			client.user.setPresence({
+				activity: {
+					name: content,
+					type: 0,
+				}
+			})
+		}
+	})
+
+})
+
+client.login(config.token)
